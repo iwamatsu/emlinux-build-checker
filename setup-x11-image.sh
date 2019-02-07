a62634137b314cc20980a0c61dfa5a2849393039
Nobuhiro Iwamatsu <iwamatsu@nigauri.org>
2019-02-07 10:20:50 +0900

    Update script for x11
    Signed-off-by: Nobuhiro Iwamatsu <iwamatsu@nigauri.org>

--- setup-x11-image.sh
+++ setup-x11-image.sh
@@ -1,31 +1,18 @@
 #!/bin/bash
 
-BASE_URI="https://gitlab.miraclelinux.com/emlinux"
-REPO_POKY="${BASE_URI}/poky.git"
-REPO_META_DEBIAN="${BASE_URI}/meta-debian.git"
-REPO_META_DEBIAN_EXTENDED="${BASE_URI}/meta-debian-extended.git"
-REPO_META_EMLINUX="${BASE_URI}/meta-emlinux.git"
-REPO_META_OPENBOX="${BASE_URI}/meta-openbox.git"
-REPO_META_OE="https://github.com/openembedded/meta-openembedded.git"
-IMAGE=core-image-openbox
+BUILD_NAME=core-image-x11
 
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
 cd ${SCRIPT_DIR}
 
-if [ ! -d repo ] ; then
-    mkdir repo
-fi
-if [ ! -d work ] ; then
-    mkdir work
-fi
-
+. __repo_info.sh
 . __repo_ctrl.sh
 
 if [ "$1" = "update" ]; then
    exit
 fi
 
-source repo/poky/oe-init-build-env work/build-${IMAGE}
+source repo/poky/oe-init-build-env work/build-${BUILD_NAME}
 
 # delete
 bitbake-layers remove-layer meta-poky
@@ -41,3 +28,5 @@ echo "PACKAGE_CLASSES = \"package_deb\"" >> ./conf/local.conf
 
 echo "DL_DIR = \"\${TOPDIR}/../downloads\"" >> ./conf/local.conf
 echo "SSTATE_DIR = \"\${TOPDIR}/../sstate-cache\"" >> ./conf/local.conf
+
+echo "BBMASK = \"meta-debian/recipes-core\"" >> ./conf/local.conf
