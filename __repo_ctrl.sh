e3bbc199bb2bb2dea7d54d451dfe0e3acd5c2b0c
Nobuhiro Iwamatsu <nobuhiro.iwamatsu@miraclelinux.com>
2019-03-07 14:44:16 +0900

    Set branch from config
    Signed-off-by: Nobuhiro Iwamatsu <nobuhiro.iwamatsu@miraclelinux.com>

--- __repo_ctrl.sh
+++ __repo_ctrl.sh
@@ -6,37 +6,37 @@ if [ ! -d work ] ; then
 fi
 
 if [ ! -e repo/meta-debian ] ; then
-    git clone -b for-thud-devel $REPO_META_DEBIAN repo/meta-debian
+    git clone -b $REPO_META_DEBIAN_BRANCH $REPO_META_DEBIAN repo/meta-debian
 else
-    cd repo/meta-debian; git remote update; git reset --hard origin/for-thud-devel; cd ../../
+    cd repo/meta-debian; git remote update; git reset --hard origin/$REPO_META_DEBIAN_BRANCH; cd ../../
 fi
 
 if [ ! -e repo/meta-debian-extended ] ; then
-    git clone $REPO_META_DEBIAN_EXTENDED repo/meta-debian-extended
+    git clone -b $REPO_META_DEBIAN_EXTENDED_BRANCH $REPO_META_DEBIAN_EXTENDED repo/meta-debian-extended
 else
-    cd repo/meta-debian-extended; git remote update; git reset --hard origin/master; cd ../../
+    cd repo/meta-debian-extended; git remote update; git reset --hard origin/$REPO_META_DEBIAN_EXTENDED_BRANCH; cd ../../
 fi
 
 if [ ! -e repo/poky ] ; then
-    git clone -b iwamatsu/libxxf86misc $REPO_POKY repo/poky
+    git clone -b $REPO_POKY_BRANCH $REPO_POKY repo/poky
 else
-    cd repo/poky; git remote update; git reset --hard origin/iwamatsu/libxxf86misc; cd ../../
+    cd repo/poky; git remote update; git reset --hard origin/$REPO_POKY_BRANCH; cd ../../
 fi
 
 if [ ! -e repo/meta-emlinux ] ; then
-    git clone $REPO_META_EMLINUX repo/meta-emlinux
+    git clone -b $REPO_META_EMLINUX_BRANCH $REPO_META_EMLINUX repo/meta-emlinux
 else
-    cd repo/meta-emlinux; git remote update; git reset --hard origin/master; cd ../../
+    cd repo/meta-emlinux; git remote update; git reset --hard origin/$REPO_META_EMLINUX_BRANCH; cd ../../
 fi
 
 if [ ! -e repo/meta-openbox ] ; then
-    git clone $REPO_META_OPENBOX repo/meta-openbox
+    git clone -b $REPO_META_OPENBOX_BRANCH $REPO_META_OPENBOX repo/meta-openbox
 else
-    cd repo/meta-openbox; git remote update; git reset --hard origin/master; cd ../../
+    cd repo/meta-openbox; git remote update; git reset --hard origin/$REPO_META_OPENBOX_BRANCH; cd ../../
 fi
 
 if [ ! -e repo/meta-openembedded ] ; then
-    git clone -b thud $REPO_META_OE repo/meta-openembedded
+    git clone -b $REPO_META_OE_BRANCH $REPO_META_OE repo/meta-openembedded
 else
-    cd repo/meta-openembedded;git remote update; git reset --hard origin/thud; cd ../../
+    cd repo/meta-openembedded;git remote update; git reset --hard origin/$REPO_META_OE_BRANCH; cd ../../
 fi
